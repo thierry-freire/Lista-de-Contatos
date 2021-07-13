46d3cd79eaba7df465d822c7d95893502284837e
thierry.freire <thierry.freire@hotmail.com>
2021-07-12 22:25:53 -0300

    Ajuste na conexão com o banco.

--- domain/db.js
+++ domain/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/lista-contatos');
+mongoose.connect('mongodb+srv://admin:adminfreire8@lista-contatos.ctm1n.mongodb.net/lista-contatos');
  
 const contatosSchema = new mongoose.Schema({
     nome: String,
